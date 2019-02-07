24e178cfcde3f47b99bc2367de8b17b1396e7b7c
ilnur.ibragimov <Ilnur.Ibragimov@e-lab.icl.kazan.ru>
2019-02-07 15:39:07 +0300

    commit

---
+++ src/components/shared/ErrorAlert.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-alert
+      :value="true"
+      type="error"
+      dismissible
+      @input="onClose"
+    >
+      {{ text }}
+    </v-alert>
+</template>
+
+
+<script>
+export default {
+  props: {
+    text: String
+  },
+  methods: {
+    onClose() {
+      this.$emit("dismissed");
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
+
+
---
+++ src/components/shared/PreloaderComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-xs-center preloader-holder">
+    <v-progress-circular
+      :size="50"
+      color="primary"
+      indeterminate
+    ></v-progress-circular>
+  </div>
+</template>
+
+
+<script>
+export default {
+    name:'preloader-component',
+    props:['loading'],
+}
+</script>
+
+<style>
+.preloader-holder {
+    position: absolute;
+    width:100%;
+    height:80%;
+    z-index: 10000;
+    background-color:rgba(255, 255, 255, 0.5); 
+}
+.v-progress-circular{
+    margin-top: 15%;
+    margin-right:49px;
+}
+</style>
+
