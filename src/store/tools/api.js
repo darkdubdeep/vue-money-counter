6a96a75c26d58151516d43325b3f58bda6f9d2d4
ilnur.ibragimov <Ilnur.Ibragimov@e-lab.icl.kazan.ru>
2019-02-26 13:14:40 +0300

    refactoring

---
+++ src/store/tools/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const developUrl = "http://192.168.99.100/api";
+const prodUrl = "http://212.109.193.230:8081/api";
+
+export default axios.create({
+  baseURL: developUrl
+});
